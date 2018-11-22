1732d7231de0942e63a166f726346a422c5e46b2
Edward Stone <ed@autotelic.com>
2018-11-21 16:19:42 -0800

    updated vars.tf with descriptions

--- vars.tf
+++ vars.tf
@@ -1,23 +1,39 @@
-variable "api_gateway_rest_api_name" {}
+variable "api_gateway_rest_api_name" {
+  description = "Name of the REST API on API gateway"
+}
 
 variable "api_gateway_endpoint_method" {
-  default = "GET"
+  default     = "GET"
+  description = "HTTP method for the API gateway proxy endpoint"
 }
 
 variable "api_gateway_deployment_stage_name" {
-  default = "development"
+  default     = "development"
+  description = "API gateway stage to publish endpoint on"
 }
 
-variable "lambda_function_name" {}
-variable "lambda_handler" {}
+variable "lambda_function_name" {
+  description = "Name of the lambda on AWS"
+}
+
+variable "lambda_handler" {
+  description = "Name of the lambda handler module"
+}
+
 variable "lambda_handler_method" {
-  default = "handler"
+  default     = "handler"
+  description = "Name of the lambda handler method"
 }
+
 variable "lambda_runtime" {
-  default = "nodejs8.10"
+  default     = "nodejs8.10"
+  description = "Node runtime for the lambda on AWS"
 }
-variable "lambda_zip_source_dir" {}
+
+variable "lambda_zip_source_dir" {
+  description = "Directory containing compiled lambda source and dependencies"
+}
 
 variable "api_gateway_lambda_iam_role" {
-  default = "api_gateway_lambda_iam_role"
+  description = "Name of the IAM role for lambda on AWS"
 }
